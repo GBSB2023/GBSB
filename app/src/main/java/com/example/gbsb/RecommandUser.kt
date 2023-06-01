2c7cc43a9ef4c238b1fe533a6c153736b3e7574b
Roh jiwnoo <50816927+best01211@users.noreply.github.com>
2023-06-01 15:07:03 +0900

    진로 추천한 내용의 타입과 검사 날짜를 저장하는 데이터
    이 내용은 FireBase를 통해 저장할 데이터 이다.

--- app/src/main/java/com/example/gbsb/RecommandUser.kt
+++ app/src/main/java/com/example/gbsb/RecommandUser.kt
@@ -1,3 +1,3 @@
 package com.example.gbsbwork
 
-data class RecommandUser(var type: String?, var date: String) // 타입과 검사 날짜를 저장하는 데이터
+data class RecommandUser(var type: String?, var date: String) // 타입과 검사 날짜를 저장하는 데이터 
