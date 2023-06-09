383603aee8f09cefc44ad7e4d1a75f0c9c77e49d
taehyeon <tedxpem@naver.com>
2023-06-09 23:03:52 +0900

    직업 버튼의 addListener부분 코드 단순화
    mainActivity의 직업 버튼 이름 변경

--- app/src/main/java/com/example/gbsb/MainActivity.kt
+++ app/src/main/java/com/example/gbsb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gbsb
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gbsb.account.AccountActivity
@@ -12,40 +13,27 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
+    private val areaIds = intArrayOf(
+        R.id.career0, R.id.career1, R.id.career2, R.id.career3,
+        R.id.career4, R.id.career5, R.id.career6, R.id.career7,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // 각 버튼에 클릭 리스너 설정
-        binding.button1.setOnClickListener {
-            navigateToMain2Activity(1)
-        }
-        binding.button2.setOnClickListener {
-            navigateToMain2Activity(2)
-        }
-        binding.button3.setOnClickListener {
-            navigateToMain2Activity(3)
-        }
-        binding.button4.setOnClickListener {
-            navigateToMain2Activity(4)
-        }
-        binding.button5.setOnClickListener {
-            navigateToMain2Activity(5)
-        }
-        binding.button6.setOnClickListener {
-            navigateToMain2Activity(6)
-        }
-        binding.button7.setOnClickListener {
-            navigateToMain2Activity(7)
-        }
-        binding.button8.setOnClickListener {
-            navigateToMain2Activity(8)
-        }
-
         initLayout()
     }
     private fun initLayout() {
+        // Connect listener to each career button
+        for (areaId in areaIds) {
+            val area = findViewById<LinearLayout>(areaId)
+            area.setOnClickListener {
+                navigateToMain2Activity(areaIds.indexOf(areaId) + 1)
+            }
+        }
+
         binding.apply {
             todoListArea.setOnClickListener {
                 if(FirebaseAuth.getInstance().currentUser?.isAnonymous == true){
